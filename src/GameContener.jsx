9a4a6e3ec628a6e1a5413ecfe3f9be05f7fb8fc3
Chinmoy Mandal <chinmoymandal925807@gmail.com>
2025-11-07 20:53:57 +0530

    implement game logic and update styling for GameContener

--- src/GameContener.jsx
+++ src/GameContener.jsx
@@ -1,10 +1,28 @@
 import styles from './GameContener.module.css';
+import { useState } from 'react';
 const GameContener = () => {
     const box = [0, 1, 2, 3, 4, 5, 6, 7, 8];
+    const boxItems = ['', '', '', '', '', '', '', '', ''];
+    const [items, setItems] = useState(boxItems);
+    const [currentPlayer, setCurrentPlayer] = useState('X');
+
+
+    const handleClick = (item) => {
+        
+        if (items[item] !== '') {
+            return;
+        }
+        const updatedItems = [...items];
+        updatedItems[item] = currentPlayer;
+            setItems(updatedItems);
+            setCurrentPlayer(pre => pre==='X' ? 'O' : 'X');
+        
+    }
+
     return (
         <div className={`${styles.game_contener}`}>
             {box.map((item) => (
-                <div key={item} className={`${styles.box}`}>{ item}</div>
+                <div onClick={()=>handleClick(item)} key={item} className={`${styles.box}`}>{ items[item]}</div>
             ))}
         </div>
     )
